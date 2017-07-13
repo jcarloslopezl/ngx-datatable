71e4f58e9a5281f1ea5c45b80bfe734484644c05
Juan Carlos López <jcarloslopezl@hotmail.com>
2017-07-13 09:50:33 -0500

    Import NgxDatatableModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpModule } from '@angular/http';
 import { UserService } from './services/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    NgxDatatableModule
   ],
   providers: [
     UserService
